eb71f0e5bd065f9916ac2f7a938a64ece419e37b
Julien Henrard <julien.henrard@outlook.com>
2022-04-22 15:26:49 +0200

    config + db + registration

--- src/orm/migrations/1590519635401-CreateBaseUsers.ts
+++ src/orm/migrations/1590519635401-CreateBaseUsers.ts
@@ -5,29 +5,29 @@ import { User } from '../entities/user/User';
 
 export class CreateBaseUsers1590519635401 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    let user = new User()
-    
-    const userRepository = getRepository(User)
+    let user = new User();
 
-    user.username = 'SuperAdmin'
-    user.name = 'Super Admin'
-    user.email = 'sadmin@btshop.com'
-    user.password = 'fW98RmT2+'
-    user.hashPassword()
-    user.role = 'ADMINISTRATOR' as Role
-    await userRepository.save(user)
+    const userRepository = getRepository(User);
 
-    user = new User()
-    user.username = 'Persilos'
-    user.name = 'Julien Henrard'
-    user.email = 'j.henrard@nexmind.be'
-    user.password = 'coucou'
-    user.hashPassword()
-    user.role = 'STANDARD' as Role
-    await userRepository.save(user)
+    user.username = 'SuperAdmin';
+    user.name = 'Super Admin';
+    user.email = 'sadmin@btshop.com';
+    user.password = 'fW98RmT2+';
+    user.hashPassword();
+    user.role = 'ADMINISTRATOR' as Role;
+    await userRepository.save(user);
+
+    user = new User();
+    user.username = 'Persilos';
+    user.name = 'Julien Henrard';
+    user.email = 'j.henrard@nexmind.be';
+    user.password = 'coucou';
+    user.hashPassword();
+    user.role = 'STANDARD' as Role;
+    await userRepository.save(user);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    console.log('Not implemented')
+    console.log('Not implemented');
   }
 }
--- src/routes/v1/shops.ts
+++ src/routes/v1/shops.ts
@@ -6,8 +6,8 @@ import { checkRole } from 'middleware/checkRole';
 
 const router = Router();
 
-router.get('/', list)
-router.put('/:id([0-9]+)', edit)
-router.post('/', [checkJwt, checkRole(['STANDARD', 'ADMINISTRATOR'], true)], create)
+router.get('/', list);
+router.put('/:id([0-9]+)', edit);
+router.post('/', [checkJwt, checkRole(['STANDARD', 'ADMINISTRATOR'], true)], create);
 
 export default router;
